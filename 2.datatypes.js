3aa6d7a7a059ff18cd76b55286096f960910cf50
Sagar Bhandari <rsaagar7200@gmail.com>
2025-09-01 20:55:26 +0545

    datatypes

---
+++ 2.datatypes.js
@@ -0,0 +1,48 @@
+// primitive
+//* 1. number (int, float)
+let a = 10
+let b = 13.5
+
+// * 2. string (char)
+let c = 'hello'
+let d = "world"
+
+// template literal 
+let e = `${c} world`
+
+// console.log(e)
+
+//*3. boolean
+ let f = true
+ let g = false
+
+//* 4. undefined 
+let h;
+
+console.log(h)
+
+//* 5. null
+let i = null
+
+//*6. symbol
+const id = Symbol('id') 
+const id2 = Symbol('id') 
+
+//* 7. bigint
+
+// console.log(Number.MAX_SAFE_INTEGER)
+
+let j = 9007199254740991n
+// console.log(j + 1n)
+// console.log(j + 100n)
+
+
+
+
+//! non primitive
+
+// object
+// array
+// function
+
+
