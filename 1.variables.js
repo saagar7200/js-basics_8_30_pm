b79d574d008f8a32f13ae14e7cb3676b15e2a09b
Sagar Bhandari <rsaagar7200@gmail.com>
2025-09-01 20:55:03 +0545

    variables

---
+++ 1.variables.js
@@ -0,0 +1,80 @@
+
+// console.log('hello world');
+
+// console.log(10)
+// console.log(10 + 20)
+
+
+//   variables declaration methods
+//  var , let , const 
+
+
+// single line comment
+
+/*
+multi
+line 
+comment
+*/
+
+var a = 10
+
+console.log(a)
+
+a = 'hello'
+
+console.log(a)
+
+var a = 'hi'
+
+console.log(a)
+
+let b = 2
+
+b = "30"
+
+// var b = 100
+
+const c = 20
+
+// c = 67
+
+let d;
+var e;
+d =300
+e =399
+const f = 12;
+
+console.log(d+e)
+
+
+// variable naming conventions
+// * camel case
+// username
+// let userName = ''
+
+
+// * snake case
+let user_name = ''
+
+// * pascal case
+let UserName = ''
+
+// u_p
+// user_pass
+
+let x=200, y
+
+x =20
+y= 30
+console.log(x,y)
+
+
+
+
+
+
+
+
+
+
